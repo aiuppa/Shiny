b87636b74535de94e46b39f4cadf93e34b809651
Anna <annaaiuppa@gmail.com>
2019-04-09 13:27:04 -0400

    adding some menu items and content, still very template-like

--- helloDashboard.R
+++ helloDashboard.R
@@ -6,11 +6,42 @@ library("shiny")
 
 #UI
 ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardHeader(title = "Exciting Title"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    #tab items must correspond with values for tab names
+    tabItems(
+      #first tab
+      tabItem(tabName = "dashboard",
+              #boxes are put in rows or columns
+              fluidRow(
+                box(plotOutput("Plot1", height = 300)),
+                box(
+                  title = "Controls", 
+                  sliderInput("slider", "Number of Observations:", 1, 100, 50)
+                  )
+              )
+      ),
+      #2nd tab
+      tabItem(tabName = "widgets",
+              h2("Widgets tab content"))
+    
+    )
+  )
 )
 
-server <- function(input, output){}
+server <- function(input, output){
+  set.seed(324)
+  histdata <- rnorm(400)
+  output$Plot1 <-renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
 
 shinyApp(ui, server)
