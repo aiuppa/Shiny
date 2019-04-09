3716496d1b1a1faf17efce3e133b627134418e55
Anna <annaaiuppa@gmail.com>
2019-04-09 13:14:17 -0400

    this is a blank dashboard

--- helloDashboard.R
+++ helloDashboard.R
@@ -2,9 +2,15 @@
 
 #install.packages("shinydashboard")
 library("shinydashboard")
+library("shiny")
 
-dashboardPage(
+#UI
+ui <- dashboardPage(
   dashboardHeader(),
   dashboardSidebar(),
   dashboardBody()
 )
+
+server <- function(input, output){}
+
+shinyApp(ui, server)
